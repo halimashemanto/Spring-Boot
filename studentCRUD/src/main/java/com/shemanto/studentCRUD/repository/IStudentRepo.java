9cb0cd807ea123eec761a8bb7a5a7706b57323b0
Suprova Shemanto <suprovashemanto@gmail.com>
2025-07-31 15:47:59 +0600

    ad

--- studentCRUD/src/main/java/com/shemanto/studentCRUD/repository/IStudentRepo.java
+++
@@ -1,9 +0,0 @@
-package com.shemanto.studentCRUD.repository;
-
-import com.shemanto.studentCRUD.entity.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IStudentRepo extends JpaRepository<Student,Long> {
-}
